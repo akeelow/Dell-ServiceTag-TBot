b226fe3516894cb3cf7f25944ae16161b13e0e3e
akeelow <akeelow@gmail.com>
2021-12-16 23:33:11 +0600

    clear code

--- dell-sttg-tgbot.py
+++ dell-sttg-tgbot.py
@@ -5,7 +5,7 @@ import re
 
 def text_recognition(image):
     reader = easyocr.Reader(["en"])
-    result = reader.readtext(image, detail = 0, paragraph = True, decoder = "wordbeamsearch", text_threshold = 0.9, contrast_ths = 0.9) 
+    result = reader.readtext(image, detail = 0, paragraph = True) 
     ocr_text = ""
     for line in result:
         ocr_text = ocr_text + line
@@ -13,8 +13,6 @@ def text_recognition(image):
     result = re.search(r'(\b[A-Z0-9]{6}\d{1}\b)', ocr_text)
 
     return result[0] if result else "err"
-
-    
 
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
@@ -33,17 +31,8 @@ async def handle_docs_photo(message):
         await message.answer(f"https://www.dell.com/support/home/ru-ru/product-support/servicetag/{tag}/overview")
 
 
-""" @dp.message_handler(regexp='(\b[A-Z0-9]{6}\d{1}\b)')
-async def start(message: types.Message):
-    await message.answer(f"https://www.dell.com/support/home/ru-ru/product-support/servicetag/{message.text}/overview")
-
-@dp.message_handler()
-async def start(message: types.Message):
-    await message.answer(f"It's not a Dell Service Tag → {message.text}")
- """    
 def main():
     executor.start_polling(dp, skip_updates=True)
     
-
 if __name__ == "__main__":
     main()
