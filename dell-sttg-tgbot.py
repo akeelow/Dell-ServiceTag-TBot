5b146092f9aa2598ada6129f04bbf74155a8d866
akeelow <akeelow@gmail.com>
2021-12-16 21:51:03 +0600

    add code

--- dell-sttg-tgbot.py
+++ dell-sttg-tgbot.py
@@ -1,6 +1,20 @@
 from aiogram import Bot, Dispatcher, executor, types
+import easyocr
 from auth import token
+import re
 
+def text_recognition(image):
+    reader = easyocr.Reader(["en"])
+    result = reader.readtext(image, detail = 0, paragraph = True, decoder = "wordbeamsearch", text_threshold = 0.9, contrast_ths = 0.9) 
+    ocr_text = ""
+    for line in result:
+        ocr_text = ocr_text + line
+    
+    result = re.search(r'(\b[A-Z0-9]{6}\d{1}\b)', ocr_text)
+
+    return result[0] if result else "err"
+
+    
 
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
@@ -9,16 +23,27 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     await message.answer("Hi! Enter Dell Service Tag")
 
-@dp.message_handler(regexp='(^[0-9A-Z]{7}?$)')
+@dp.message_handler(content_types=['photo'])
+async def handle_docs_photo(message):
+    await message.photo[-1].download('temp.jpg')
+    tag = text_recognition(image = "temp.jpg")
+    if tag == "err":
+        await message.answer("NOT FOUND Dell Service Tag")
+    else:
+        await message.answer(f"https://www.dell.com/support/home/ru-ru/product-support/servicetag/{tag}/overview")
+
+
+""" @dp.message_handler(regexp='(\b[A-Z0-9]{6}\d{1}\b)')
 async def start(message: types.Message):
     await message.answer(f"https://www.dell.com/support/home/ru-ru/product-support/servicetag/{message.text}/overview")
 
 @dp.message_handler()
 async def start(message: types.Message):
     await message.answer(f"It's not a Dell Service Tag → {message.text}")
-    
+ """    
 def main():
     executor.start_polling(dp, skip_updates=True)
+    
 
 if __name__ == "__main__":
     main()
