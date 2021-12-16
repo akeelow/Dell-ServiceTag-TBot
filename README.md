828641b9eff73effbea6532ed925e0367aef5260
akeelow <akeelow@gmail.com>
2021-12-16 10:46:54 +0600

    created bot

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Dell-ServiceTag-TBot
 
 Dell Service Tag Link Generator Telegram Bot
+
+For start
+ - create file auth.py and write: token = "TELEGRAM BOT TOKEN"
+ - run terminal and enter command: pip instal aiogram
---
+++ dell-sttg-tgbot.py
@@ -0,0 +1,31 @@
+from aiogram import Bot, Dispatcher, executor, types
+from auth import token
+
+
+bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    await message.answer("Hi! Enter Dell Service Tag")
+
+@dp.message_handler(regexp='(^[0-9A-Z]{7}?$)')
+async def start(message: types.Message):
+    await message.answer(f"https://www.dell.com/support/home/ru-ru/product-support/servicetag/{message.text}/overview")
+
+@dp.message_handler()
+async def start(message: types.Message):
+    await message.answer(f"It's not a Dell Service Tag → {message.text}")
+    
+
+
+
+def get_link(tag="35D51N2"):
+    print(f"https://www.dell.com/support/home/ru-ru/product-support/servicetag/{token}/overview")
+
+def main():
+    #get_link(tag="4444")
+    executor.start_polling(dp, skip_updates=True)
+
+if __name__ == "__main__":
+    main()
