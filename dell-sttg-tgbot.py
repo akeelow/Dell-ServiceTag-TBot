22a692fab72296a2b32bff22b494a0a6d92d025b
akeelow <akeelow@gmail.com>
2021-12-20 23:12:22 +0600

    save

--- dell-sttg-tgbot.py
+++ dell-sttg-tgbot.py
@@ -1,9 +1,23 @@
 from aiogram import Bot, Dispatcher, executor, types
-import easyocr
+import pytesseract
+from PIL import Image
+#import easyocr
 from auth import token
 import re
 
-def text_recognition(image):
+def text_recognition():
+    img = Image.open("temp.jpg")
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    #custom_config = r'--oem 3 --psm 8'
+
+    text = pytesseract.image_to_string(img)#, config = custom_config)
+    print("-----------" + text)
+    result = re.search(r'(\b[A-Z0-9]{6}\d{1}\b)', text)
+    print(result[0] if result else "err")
+    return result[0] if result else "err"
+
+# OCR with EasyOCR
+""" def text_recognition(image):
     reader = easyocr.Reader(["en"])
     result = reader.readtext(image, detail = 0, paragraph = True) 
     ocr_text = ""
@@ -12,7 +26,7 @@ def text_recognition(image):
     
     result = re.search(r'(\b[A-Z0-9]{6}\d{1}\b)', ocr_text)
 
-    return result[0] if result else "err"
+    return result[0] if result else "err" """
 
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
@@ -24,7 +38,7 @@ async def start(message: types.Message):
 @dp.message_handler(content_types=['photo'])
 async def handle_docs_photo(message):
     await message.photo[-1].download('temp.jpg')
-    tag = text_recognition(image = "temp.jpg")
+    tag = text_recognition()
     if tag == "err":
         await message.answer("NOT FOUND Dell Service Tag")
     else:
